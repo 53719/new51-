72795222a8cd408b589ee8548c70c251f9100c7e
chen1996 <1324417058@qq.com>
2020-06-21 17:47:34 +0800

    安装 vant ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入 vant ui
+// 1. 引入 vant ui 组件库
+import Vant from 'vant';
+// 2. 引入对应的 css 文件
+import 'vant/lib/index.css';
+// 3. 注册组件
+Vue.use(Vant);
+
+
 Vue.config.productionTip = false
 
 new Vue({
