1a21dac704f06cda0c6bdbcf1a4d287a80f6c4b3
chen1996 <1324417058@qq.com>
2020-06-23 10:07:37 +0800

    完善登录页面的非空和格式错误判断

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,14 +10,14 @@
     <AuthInput 
         textPlaceholder="用户名/手机号码"
         type='text'
-        rule='^\d{5,11}$'
+        :rule='ruleUsername'
         errMsg='请输入正确的手机号码'
          @valChanged="setUsername"
          />
     <AuthInput
           textPlaceholder="密码"
           type="password"
-          rule="^.{3,9}$"
+          :rule="rulePassword"
           errMsg="请输入三到九位的密码"
            @valChanged="setPassword"
           />
@@ -34,7 +34,9 @@ export default {
   data () {
     return{
             username: '',
-            password: ''
+            password: '',
+            ruleUsername:'^\\d{5,11}$',
+            rulePassword:'^.{3,9}$'
     }
   },
   components: {
@@ -43,11 +45,25 @@ export default {
   },
   methods:{
      login() {
-            console.log('父组件接收到按钮子组件的点击事件, 触发自己的登录函数'); 
-            // 登录请求就需要 用户名和密码的数据
-            // 这里直接通过 this.username / this.password 拿到用户输入数据
-            console.log('用户名', this.username);
-            console.log('密码', this.password); 
+            // console.log('父组件接收到按钮子组件的点击事件, 触发自己的登录函数'); 
+            // // 登录请求就需要 用户名和密码的数据
+            // // 这里直接通过 this.username / this.password 拿到用户输入数据
+            // console.log('用户名', this.username);
+            // console.log('密码', this.password); 
+            if(!this.username || !this.password){
+              this.$toast.fail('请输入完整信息')
+              return
+            }
+            var regExpUsername= new RegExp(this.ruleUsername)
+            if(!regExpUsername.test(this.username)){
+              this.$toast.fail('账户信息格式错误')
+              return
+            }
+            var regExpPassword= new RegExp(this.rulePassword)
+            if(!regExpPassword.test(this.password)){
+              this.$toast.fail('密码格式错误')
+              return
+            }
 
             // 真正发送登录请求
             this.$axios({
