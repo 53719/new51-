1e2ff947e1ccffe85a4fc644bc9f3d1314553a86
chen1996 <1324417058@qq.com>
2020-06-23 11:00:25 +0800

    个人中心上半部分完成

--- src/views/personal/Index.vue
+++ src/views/personal/Index.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>个人中心</div>
+  <div>
+    <div class="profile">
+      <img src="@/assets/logo1.png" alt class="avatar" />
+      <div class="info">
+        <div class="name">
+          <span class="iconfont iconxingbienv"></span>
+          舔狗
+        </div>
+        <div class="data">2020-6-23</div>
+      </div>
+      <a href="#" class="iconfont iconjiantou1"></a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-
+.profile {
+  display: flex;
+  align-items: center;
+  padding: 6.667vw;
+  .avatar {
+    width: 19.444vw;
+    height: 19.444vw;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+  .info {
+    flex: 1;
+    padding-left: 2.778vw;
+    font-size: 3.889vw;
+    .name {
+        .iconxingbienv{
+            color: hotpink;
+        }
+    }
+    .data{
+        padding-top: 1.389vw;
+        color: gray;
+    }
+  }
+}
 </style>
