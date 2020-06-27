2816116419884c4c4e13a24525817929ef905d9e
chen1996 <1324417058@qq.com>
2020-06-27 16:19:33 +0800

    添加首页顶部布局

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="headerWrapper">
+      <span class="iconfont iconnew"></span>
+      <div class="search">
+          <span class="iconfont iconsearch"></span>
+          搜索新闻
+      </div>
+    <span class="iconfont iconwode"></span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.headerWrapper {
+    display: flex;
+    align-items: center;
+    background-color: red;
+    color: white;
+    padding: 1.111vw 2.778vw;
+    .iconnew {
+        font-size: 15vw;
+    }
+    .search {
+        flex: 1;
+        background-color: rgba(255, 255, 255, .4);
+        margin: 0 5.556vw 0 3.889vw;
+        text-align: center;
+        height: 11.111vw;
+        line-height: 11.111vw;
+        border-radius: 5.556vw;
+    }
+    .iconwode {
+        font-size: 8.889vw;
+        opacity: .6;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,22 @@
 <template>
   <div class="home">
-    <router-link to="/login">登录页</router-link>
-    <router-link to="/Register">注册页</router-link>
-    <router-link to="/personal">个人中心</router-link>
-    <div v-for="(item,index) in list" :key="index">{{item.title}}</div>
+    <HomeHeader />
   </div>
 </template>
 
 <script>
+import HomeHeader from '@/components/HomeHeader'
 export default {
   data() {
     return {
       list: []
     };
   },
-  mounted() {
-    this.$axios({
-      url: "/post",
-      method: "get",
-      params: {
-        category: 7
-      }
-    }).then(res => {
-      // 以前 jq 的 success 写在这里就 ok 了
-      // 可以接收到请求结果 res
-      // 我们想要的数据就在 res.data 里面
-      console.log(res.data);
-      // 对于这个接口来说, 文章的列表在 res.data.data 下面
-      // 如果我想要渲染文章列表, 赋值到 data 属性中即可
-      this.list = res.data.data;
-    }).catch(err=>{
-      // console.log('这里是错误处理函数');
-      this.$toast.fail('请求失败')
-    })
+  components: {
+   HomeHeader
   }
 };
 </script>
 <style scoped lang='less'>
-/* 可以限制css的影响范围 */
-/* 只影响当前组件 */
-.iconsearch {
-  color: red;
-  font-size: 30px;
-}
 </style>>
 
