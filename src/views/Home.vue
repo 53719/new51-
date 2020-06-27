1bcb12bd11a66f0d37becd6a306414560bceed8f
chen1996 <1324417058@qq.com>
2020-06-27 17:09:02 +0800

    首页数据获取优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,8 @@
     <HomeHeader />
 
     <van-tabs v-model="activeTab">
-      <van-tab
-        :title="category.name"
-        v-for="category in categoriesList"
-        :key="category.id"
-      >
-        <div v-for="post in postList" :key="post.id">
-          {{post.title}}
-        </div>
+      <van-tab :title="category.name" v-for="category in categoriesList" :key="category.id">
+        <div v-for="post in postList" :key="post.id">{{post.title}}</div>
       </van-tab>
     </van-tabs>
   </div>
@@ -26,38 +20,46 @@ export default {
     return {
       activeTab: 0,
       categoriesList: [],
-      postList:[]
+      postList: []
     };
   },
 
   created() {
-    this.$axios({
-      url: "/category"
-    }).then(res => {
-      console.log(res.data);
-      this.categoriesList = res.data.data;
-      // 只有获取完了栏目数据, 才可以开始获取文章数据
-      // 获取文章需要发送一个 ajax 请求, 建议另外封装函数
-      this.getPost()
-    });
+    //页面创建之后,获取栏目
+    this.getCategories();
+  },
+  computed: {
+    categoryId() {
+      // 当前激活分类,是在分类列表中拿出当前激活的索引
+      const currentCategory = this.categoriesList[this.activeTab];
+      // 从中获取 id
+      return currentCategory.id;
+    }
   },
   watch: {
-    activeTab(){
-      this.getPost()
+    activeTab() {
+      this.getPost();
     }
   },
 
   methods: {
+        getCategories() {
+      this.$axios({
+        url: '/category'
+      }).then(res=>{
+        console.log(res.data);
+        this.categoriesList = res.data.data
+        // 只有获取完了栏目数据, 才可以开始获取文章数据
+        // 获取文章需要发送一个 ajax 请求, 建议另外封装函数
+        this.getPost()
+      })
+    },
     getPost() {
-      // 当前激活分类,是在分类列表中拿出当前激活的索引
-      const currentCategory = this.categoriesList[this.activeTab];
-      // 从中获取 id
-      const categoryId = currentCategory.id;
 
       this.$axios({
         url: "/post",
         params: {
-          category: categoryId
+          category: this.categoryId
         }
       }).then(res => {
         console.log(res.data);
