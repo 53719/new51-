c72a87adfffab429cdc057541c7177f3cd1ed2b9
chen1996 <1324417058@qq.com>
2020-06-27 16:57:24 +0800

    根据当前激活分类获取文章列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,69 @@
 <template>
   <div>
     <HomeHeader />
-      
-    <van-tabs v-model="activeTab">
 
-      <van-tab 
+    <van-tabs v-model="activeTab">
+      <van-tab
         :title="category.name"
         v-for="category in categoriesList"
         :key="category.id"
       >
-         这里应该放 {{category.name}} 新闻
+        <div v-for="post in postList" :key="post.id">
+          {{post.title}}
+        </div>
       </van-tab>
     </van-tabs>
   </div>
 </template>
 
 <script>
-import HomeHeader from '@/components/HomeHeader'
+import HomeHeader from "@/components/HomeHeader";
 export default {
-    components: {
-   HomeHeader
+  components: {
+    HomeHeader
   },
   data() {
     return {
-       activeTab: 0,
-       categoriesList: []
+      activeTab: 0,
+      categoriesList: [],
+      postList:[]
     };
   },
 
-  created(){
-       this.$axios({
-      url: '/category'
-    }).then(res=>{
+  created() {
+    this.$axios({
+      url: "/category"
+    }).then(res => {
       console.log(res.data);
-      this.categoriesList = res.data.data
-    })
+      this.categoriesList = res.data.data;
+      // 只有获取完了栏目数据, 才可以开始获取文章数据
+      // 获取文章需要发送一个 ajax 请求, 建议另外封装函数
+      this.getPost()
+    });
+  },
+  watch: {
+    activeTab(){
+      this.getPost()
+    }
+  },
+
+  methods: {
+    getPost() {
+      // 当前激活分类,是在分类列表中拿出当前激活的索引
+      const currentCategory = this.categoriesList[this.activeTab];
+      // 从中获取 id
+      const categoryId = currentCategory.id;
+
+      this.$axios({
+        url: "/post",
+        params: {
+          category: categoryId
+        }
+      }).then(res => {
+        console.log(res.data);
+        this.postList = res.data.data;
+      });
+    }
   }
 };
 </script>
