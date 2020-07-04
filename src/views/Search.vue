e1c578a7de1a885755836fe443aea591a8a29df5
chen1996 <1324417058@qq.com>
2020-07-04 14:52:54 +0800

    搜索功能完成

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,15 +4,80 @@
       <span class="iconfont iconjiantou2" @click="$router.back()"></span>
       <div class="searchBox">
         <span class="iconfont iconsearch"></span>
-        <input type="text">
+        <input type="text" v-model="keyword" placeholder="请输入关键字" />
       </div>
-      <div class="btn">搜索</div>
+      <div class="btn" @click="search">搜索</div>
+    </div>
+
+    <div class="suggestions" v-if="results.length == 0">
+      <h2 class="title">历史搜索</h2>
+      <div class="history list">
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+      </div>
+      <h2 class="title">热门搜索</h2>
+      <div class="hot list">
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+        <div class="item">美女</div>
+        <div class="item">关晓彤</div>
+        <div class="item">英语</div>
+      </div>
+    </div>
+
+    <div class="results">
+        <PostItem :postData='item' v-for="item in results" :key="item.id"/>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import PostItem from "@/components/PostItem";
+export default {
+  components: {
+    PostItem
+  },
+  data() {
+    return {
+      keyword: "",
+      results: []
+    };
+  },
+  methods: {
+      search(){
+          // 1 拿到当前的输入关键字, 发送请求  没有拿到数据就返回
+          if(!this.keyword){
+              return
+          }
+          this.$axios({
+              url: "/post_search",
+              params:{
+                  keyword:this.keyword
+              }
+          }).then(res=>{
+              console.log(res.data);
+              // 2. 将获取到的搜索结果
+                // 其实就是一个文章数组
+                // 存放到 data
+              this.results=res.data.data
+          })
+      }
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -29,16 +94,31 @@ export default {};
     height: 10.556vw;
     align-items: center;
     padding: 0 2.778vw;
-    .iconsearch{
-        margin-right: 2.778vw;
-        line-height: 10.556vw;
+    .iconsearch {
+      margin-right: 2.778vw;
+      line-height: 10.556vw;
     }
-    input{
-        border: none;
+    input {
+      border: none;
     }
   }
-  .btn{
-      font-size: 3.889vw;
+  .btn {
+    font-size: 3.889vw;
   }
+}
+.suggestions {
+    padding: 20px;
+    .title {
+        font-size: 16px;
+    }
+    .list {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 10px 0;
+    }
+    .item {
+        margin: 0 5px;
+        padding: 5px 5px;
+    }
 }
 </style>
