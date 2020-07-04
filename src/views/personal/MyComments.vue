912969b5bf77914b6c1257ec934056407b454af3
chen1996 <1324417058@qq.com>
2020-07-04 15:47:28 +0800

    获取数据并且渲染

--- src/views/personal/MyComments.vue
+++ src/views/personal/MyComments.vue
@@ -1,35 +1,23 @@
 <template>
   <div>
     <TopNav titleText="我的跟帖" />
+    <!-- 列表容器 -->
     <div class="list">
       <!-- 每个 item 都是一条评论 -->
       <!-- 我的评论里面有两种样式
       第一种直接回复文章没有 parent-->
-      <div class="item">
-        <div class="data">1996-10-29</div>
-        <div class="content">阿信是张信哲吗?张信哲是张学友的弟弟吗?</div>
-        <div class="origin">原文:阿信分享xxxxx</div>
-      </div>
-       <!-- 第二种 回复别人的评论, 带有 parent  -->
-       <div class="item">
-         <div class="data">1996-10-29</div>
-         <div class="parentWrapper">
-          <div class="info">
-            回复: xxx
-          </div>
-          <div class="content">
-            啦啦啦啦啦啦啦
-          </div>
+      <!-- 第二种 回复别人的评论, 带有 parent  -->
+      <div class="item" v-for="item in commentList" :key="item.id">
+        <div class="date">{{item.create_date.split('T')[0]}}</div>
+
+        <div class="parentWrapper" v-if="item.parent">
+          <div class="info">回复: {{item.parent.user.nickname}}</div>
+          <div class="content">{{item.parent.content}}</div>
         </div>
 
-        <div class="content">
-          阿信是张信哲吗, 张信哲是张学友的弟弟吗?
-        </div>
-        <div class="origin">
-          原文: 阿信分享xxxxx
-        </div>
-      
-       </div>
+        <div class="content">{{item.content}}</div>
+        <div class="origin">原文: {{item.post.title}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -39,6 +27,19 @@ import TopNav from "@/components/TopNav";
 export default {
   components: {
     TopNav
+  },
+  data() {
+    return {
+      commentList: []
+    };
+  },
+  created() {
+    this.$axios({
+      url: "/user_comments"
+    }).then(res => {
+      console.log(res.data);
+      this.commentList = res.data.data;
+    });
   }
 };
 </script>
@@ -60,6 +61,9 @@ export default {
     padding-top: 10px;
     font-size: 14px;
     color: #999;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
   .parentWrapper {
     background: #e4e4e4;
