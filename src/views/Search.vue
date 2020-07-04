7c916d163caebf7be768078f06416560ef354d1d
chen1996 <1324417058@qq.com>
2020-07-04 15:11:16 +0800

    搜索的历史记录功能

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -12,20 +12,26 @@
     <div class="suggestions" v-if="results.length == 0">
       <h2 class="title">历史搜索</h2>
       <div class="history list">
-       <div @click="sendSuggestion(item)" class="item" v-for="(item,index) of history" :key="index">
-           {{item}}
-       </div>
+        <div
+          @click="sendSuggestion(item)"
+          class="item"
+          v-for="(item,index) of history"
+          :key="index"
+        >{{item}}</div>
       </div>
       <h2 class="title">热门搜索</h2>
       <div class="hot list">
-           <div @click="sendSuggestion(item)" class="item" v-for="(item,index) of hot" :key="index">
-           {{item}}
-       </div>
+        <div
+          @click="sendSuggestion(item)"
+          class="item"
+          v-for="(item,index) of hot"
+          :key="index"
+        >{{item}}</div>
       </div>
     </div>
 
     <div class="results">
-        <PostItem :postData='item' v-for="item in results" :key="item.id"/>
+      <PostItem :postData="item" v-for="item in results" :key="item.id" />
     </div>
   </div>
 </template>
@@ -40,54 +46,64 @@ export default {
     return {
       keyword: "",
       results: [],
-      history:[
-          '美女','关晓彤','华为'
-      ],
-      hot:[
-          '美女','阿信','华为'
-      ]
+      // 1. 一个管理历史的数组
+      history: [],
+      hot: ["美女", "阿信", "华为"]
     };
   },
-  watch:{
-      keyword(newVal){
-          if(!newVal){
-              this.results=[]
-          }
+  created() {
+    const historyStorage = localStorage.getItem("history");
+    if (historyStorage) {
+      this.history = JSON.parse(historyStorage);
+    }
+  },
+  watch: {
+    keyword(newVal) {
+      if (!newVal) {
+        this.results = [];
       }
+    },
+    history() {
+            // 3. 监听到历史发生变化, 存放到本地储存
+            localStorage.setItem('history', JSON.stringify(this.history))
+        }
   },
   methods: {
-      search(){
-          // 1 拿到当前的输入关键字, 发送请求  没有拿到数据就返回
-          if(!this.keyword){
-              return
-          }
-          this.$axios({
-              url: "/post_search",
-              params:{
-                  keyword:this.keyword
-              }
-          }).then(res=>{
-              console.log(res.data);
-              // 2. 将获取到的搜索结果
-                // 其实就是一个文章数组
-                // 存放到 data
-              this.results=res.data.data
-          });
-      },
-      sendSuggestion(item){
-          this.keyword=item,
-          this.search()
-      },
-      goBack(){
-          if(this.results.length==0){
-              //如果本来就是没有结果,才返回上一页
-              this.$route.back()
-          }else{
-               // 如果已经有搜索结果就应该只是清空结果
-                // 还是留在搜索页
-                this.keyword = ''
-          }
+    search() {
+      if (!this.keyword) {
+        return;
       }
+      this.$axios({
+        url: "/post_search",
+        params: {
+          keyword: this.keyword
+        }
+      }).then(res => {
+        console.log(res.data);
+
+        // 2. 搜索成功时, 如果当前关键词
+        // 还没有记录到历史中, 就放进去
+        if (this.history.indexOf(this.keyword) == -1) {
+          this.history.push(this.keyword);
+        }
+        console.log("当前历史记录");
+        console.log(this.history);
+        this.results = res.data.data;
+      });
+    },
+    sendSuggestion(item) {
+      (this.keyword = item), this.search();
+    },
+    goBack() {
+      if (this.results.length == 0) {
+        //如果本来就是没有结果,才返回上一页
+        this.$route.back();
+      } else {
+        // 如果已经有搜索结果就应该只是清空结果
+        // 还是留在搜索页
+        this.keyword = "";
+      }
+    }
   }
 };
 </script>
@@ -119,18 +135,18 @@ export default {
   }
 }
 .suggestions {
-    padding: 20px;
-    .title {
-        font-size: 16px;
-    }
-    .list {
-        display: flex;
-        flex-wrap: wrap;
-        margin: 10px 0;
-    }
-    .item {
-        margin: 0 5px;
-        padding: 5px 5px;
-    }
+  padding: 20px;
+  .title {
+    font-size: 16px;
+  }
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 10px 0;
+  }
+  .item {
+    margin: 0 5px;
+    padding: 5px 5px;
+  }
 }
 </style>
