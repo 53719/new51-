6d29e2c80b000eea2a7dae33111479b5a43e34af
chen1996 <1324417058@qq.com>
2020-07-04 16:06:59 +0800

    栏目管理的交互功能

--- src/views/personal/ManageCategory.vue
+++ src/views/personal/ManageCategory.vue
@@ -4,10 +4,22 @@
     <div class="mainWrapper">
       <h2>点击删除以下频道</h2>
       <div class="enable list">
-        <div class="item" v-for="item in enableList" :key="item.id">{{item.name}}</div>
+        <div
+          class="item"
+          v-for="(item,index) in enableList"
+          :key="item.id"
+          @click="disableCategory(index)"
+        >{{item.name}}</div>
       </div>
       <h2>点击添加以下频道</h2>
-      <div class="list disable"></div>
+      <div class="list disable">
+        <div
+          class="item"
+          v-for="(item, index) in disableList"
+          :key="item.id"
+          @click="enableCategory(index)"
+        >{{item.name}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +32,8 @@ export default {
   },
   data() {
     return {
-      enableList: []
+      enableList: [],
+      disableList: []
     };
   },
   created() {
@@ -30,6 +43,24 @@ export default {
       console.log(res.data);
       this.enableList = res.data.data;
     });
+  },
+  methods: {
+    disableCategory(index) {
+      // 如果发现激活项只剩下一个, 就不在移除了
+      if (this.enableList.length == 1) {
+        return;
+      }
+
+      // 拿到了该转移位置的元素索引之后
+      // 1. 将这个元素放入另一个数组中
+      this.disableList.push(this.enableList[index]);
+      // 2. 将这个元素在这个数组中删除
+      this.enableList.splice(index, 1);
+    },
+    enableCategory(index) {
+      this.enableList.push(this.disableList[index]);
+      this.disableList.splice(index, 1);
+    }
   }
 };
 </script>
