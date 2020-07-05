9cf78f9704642d641ef63f897241b8940ee7cea1
chen1996 <1324417058@qq.com>
2020-07-05 09:20:55 +0800

    栏目管理同步到本地缓存

--- src/views/personal/ManageCategory.vue
+++ src/views/personal/ManageCategory.vue
@@ -36,13 +36,33 @@ export default {
       disableList: []
     };
   },
+  watch: {
+    enableList() {
+      // 如果数组有变化 马上放入本地储存
+      // 注意只能存放字符串 需要用JSON.stringify转换
+      localStorage.setItem("enable", JSON.stringify(this.enableList));
+    },
+    disableList() {
+      localStorage.setItem("disable", JSON.stringify(this.disableList));
+    }
+  },
   created() {
-    this.$axios({
-      url: "/category"
-    }).then(res => {
-      console.log(res.data);
-      this.enableList = res.data.data;
-    });
+    //在监听器中 已经同步在本地
+    //进入页面时,如果本地存储有数据,就直接拿出来恢复
+    //如果没有才发送请求
+    const enable = localStorage.getItem("enable");
+    const disable = localStorage.getItem("disable");
+    if (enable) {
+      this.enableList = JSON.parse(enable);
+      this.disableList = JSON.parse(disable);
+    } else {
+      this.$axios({
+        url: "/category"
+      }).then(res => {
+        console.log(res.data);
+        this.enableList = res.data.data;
+      });
+    }
   },
   methods: {
     disableCategory(index) {
